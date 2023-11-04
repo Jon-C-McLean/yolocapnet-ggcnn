7a1764621572fd083c28ee2046cf5cbc3c8e1ac5
Jon McLean <jon@mclean.one>
2023-11-04 20:11:59 +1100

    Removed unused fc layer

--- yolocapnet/yolo.py
+++ yolocapnet/yolo.py
@@ -4,11 +4,10 @@ from torch import nn
 from yolocapnet.layers.conv import Conv
 
 class Darknet53(nn.Module):
-    def __init__(self, block, num_class=10, init_weights=True, num_boxes = 10, include_fc_layer=False): # Change to allow dev to specify weight initialization func
+    def __init__(self, block, num_class=10, init_weights=True, num_boxes = 10): # Change to allow dev to specify weight initialization func
         super(Darknet53, self).__init__()
         self.num_class = num_class
         self.num_boxes = num_boxes
-        self.include_fc_layer = include_fc_layer
 
         if init_weights:
             self.__initialize_weights()
@@ -35,8 +34,6 @@ class Darknet53(nn.Module):
             Conv(512, 1024, kernel=3, stride=2),
             *self.__get_layer(block, 1024, blocks=4),
         )
-
-        self.fc = nn.Linear(1024, self.num_class)
 
     def forward(self, x):
         route_1 = self.map_1(x)
